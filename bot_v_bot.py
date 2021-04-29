1b414de804e371df85de82c7279a401cdbf106cf
Garrett Anderson <g@devnull.rip>
2021-04-28 18:02:37 -0700

    IDK what happened here

---
+++ bot_v_bot.py
@@ -0,0 +1,27 @@
+import time
+
+from dlgo import agent
+from dlgo import gotypes
+from dlgo import goboard_slow as goboard
+from dlgo.utils import print_board, print_move
+
+
+def main():
+  board_size = 9
+  game = goboard.GameState.new_game(board_size)
+  bots = {
+    gotypes.Player.black: agent.naive.RandomBot(),
+    gotypes.Player.white: agent.naive.RandomBot(),
+  }
+  while not game.is_over():
+    time.sleep(0.3)
+
+    print(chr(27) + "[2J")
+    print_board(game.board)
+    bot_move = bots[game.next_player].select_move(game)
+    print_move(game.next_player, bot_move)
+    game = game.apply_move(bot_move)
+
+
+if __name__ == '__main__':
+  main()
